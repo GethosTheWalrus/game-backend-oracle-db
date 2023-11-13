abe232ec6a81e04fa4182b90dfcb6e106bf28e9b
Mike Toscano <michael.andrew.toscano@gmail.com>
2023-11-13 15:27:03 -0500

    add database scripts

---
+++ PLAYER_SCORES.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - Monday-November-13-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for View PLAYER_SCORES
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE JSON RELATIONAL DUALITY VIEW "C##GAMEDB"."PLAYER_SCORES"  AS 
+  select json {'username' : u.USERNAME,
+             'id'   : u.ID,
+             'scores' :
+               [ select json {'id' : s.ID, 'value' : s.VALUE}
+                 from   SCORES s with insert update delete
+                 where  s.USER_ID = u.ID  
+               ]}
+from USERS u with insert update delete
+;
---
+++ SCORES.sql
@@ -0,0 +1,33 @@
+--------------------------------------------------------
+--  File created - Monday-November-13-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table SCORES
+--------------------------------------------------------
+
+  CREATE TABLE "C##GAMEDB"."SCORES" 
+   (	"ID" NUMBER GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE , 
+	"VALUE" NUMBER(38,0) DEFAULT 0, 
+	"USER_ID" NUMBER(38,0)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table SCORES
+--------------------------------------------------------
+
+  ALTER TABLE "C##GAMEDB"."SCORES" MODIFY ("ID" NOT NULL ENABLE);
+  ALTER TABLE "C##GAMEDB"."SCORES" MODIFY ("VALUE" NOT NULL ENABLE);
+  ALTER TABLE "C##GAMEDB"."SCORES" MODIFY ("USER_ID" NOT NULL ENABLE);
+  ALTER TABLE "C##GAMEDB"."SCORES" ADD CONSTRAINT "SCORES_PK" PRIMARY KEY ("ID")
+  USING INDEX "SYS"."SCORES_ID_IDX"  ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table SCORES
+--------------------------------------------------------
+
+  ALTER TABLE "C##GAMEDB"."SCORES" ADD CONSTRAINT "FK_USERS" FOREIGN KEY ("USER_ID")
+	  REFERENCES "C##GAMEDB"."USERS" ("ID") ENABLE;
