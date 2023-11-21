75b1118418082ee8c94ac4cf173c3ceb8fa4ee20
Mike Toscano <michael.andrew.toscano@gmail.com>
2023-11-21 14:34:53 -0500

    fix PLSQL startup scripts. They were not being created on startup due to some missing / characters.

--- database-scripts/06.sql
+++ database-scripts/06.sql
@@ -1,4 +1,5 @@
 ALTER SESSION SET CONTAINER=FREEPDB1;
+
 -- create the queue and queue table to support the chat
 BEGIN
   DBMS_AQADM.CREATE_QUEUE_TABLE(
@@ -12,6 +13,7 @@ BEGIN
   DBMS_AQADM.START_QUEUE(
     QUEUE_NAME         => 'GAMEDB.GAME_CHAT_QUEUE');
 END;
+/
 -- create a PLSQL function to enqueue a message
 create or replace function GAMEDB.ENQUEUEMESSAGE
    ( message IN varchar2 ) 
@@ -32,23 +34,27 @@ BEGIN
     msg_text := utl_raw.cast_to_varchar2(l_event_msg);
     return msg_text;
 END ENQUEUEMESSAGE;
+/
 -- create a PLSQL function to dequeue a message
-create or replace function GAMEDB.DEQUEUEMESSAGE 
-    RETURN varchar2 
-AS
+create or replace function GAMEDB.DEQUEUEMESSAGE return varchar2 as
     l_dequeue_options     dbms_aq.dequeue_options_t;
     l_message_properties  dbms_aq.message_properties_t;
     l_message_handle      raw(16);
     l_event_msg           raw(32767);
     msg_text              varchar2(32767);
+    queue_length          number;
 BEGIN
-    dbms_aq.dequeue(queue_name => 'GAMEDB.GAME_CHAT_QUEUE',
-                   dequeue_options => l_dequeue_options,
-                   message_properties => l_message_properties,
-                   payload => l_event_msg,
-                   msgid => l_message_handle);
-
-    msg_text := utl_raw.cast_to_varchar2(l_event_msg);
+    msg_text := '{}';
+    SELECT COUNT(*) INTO queue_length FROM GAMEDB.GAME_CHAT_TABLE;
+    IF queue_length > 0 THEN
+        dbms_aq.dequeue(queue_name => 'GAMEDB.GAME_CHAT_QUEUE',
+                       dequeue_options => l_dequeue_options,
+                       message_properties => l_message_properties,
+                       payload => l_event_msg,
+                       msgid => l_message_handle);
+    
+        msg_text := utl_raw.cast_to_varchar2(l_event_msg);
+    END IF;
     return msg_text;
 END DEQUEUEMESSAGE;
-COMMIT;
+/
