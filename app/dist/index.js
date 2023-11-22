2f600e8851c1d35e42d01547a37dc684528a8d31
Mike Toscano <michael.andrew.toscano@gmail.com>
2023-11-22 16:12:56 -0500

    separate out http and web socket server functionality into a static class in order to declutter index.ts

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,43 +1,8 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
-const cors_1 = __importDefault(require("cors"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const express_1 = __importDefault(require("express"));
-const playersRouter_1 = require("./routes/playersRouter");
-const chatRouter_1 = require("./routes/chatRouter");
-const environment_1 = require("./environment/environment");
-const socket_service_1 = require("./services/socket.service");
-const oracleQueue_service_1 = require("./services/oracleQueue.service");
-// configure the server
-const app = (0, express_1.default)();
-const server = require('http').createServer(app);
-const io = (0, socket_service_1.initSocket)(server);
-// connect the additional modules and the routers
-app.use(body_parser_1.default.json());
-app.use((0, cors_1.default)());
-app.use(playersRouter_1.playersRouter);
-app.use(chatRouter_1.chatRouter);
-// start a background task to poll for new chat messages
-setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
-    // dequeue our messages
-    let batchOfMessages = yield (0, oracleQueue_service_1.dequeueMany)(environment_1.CHAT_POLL_MESSAGE_COUNT);
-    // broadcast the messages to our connected clients
-    console.log('broadcasting ', batchOfMessages.length, ' messages');
-    batchOfMessages.forEach((message) => {
-        io.emit('chat', message);
-    });
-}), environment_1.CHAT_POLL_INTERVAL);
-server.listen(3000);
+const server_service_1 = require("./services/server.service");
+// Start the background process to poll for chat messages
+server_service_1.ServerService.startBackgroundChatPollProcess();
+// Start the HTTP and web socket servers
+server_service_1.ServerService.startServer();
